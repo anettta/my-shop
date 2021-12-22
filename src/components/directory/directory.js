efc9a0059a5fe33f8762e956686eacbb67579c87
Anya <annagapyuk@gmail.com>
2021-12-21 23:02:25 -0500

    added size prop

--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -9,33 +9,33 @@ class Directory extends React.Component {
     this.state = {
       sections: [
         {
-          title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          title: "puzzles",
+          // imageUrl: "../directory/puzzle.jpeg",
           id: 1,
           linkUrl: "shop/hats",
         },
         {
           title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          // imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           id: 2,
           linkUrl: "shop/jackets",
         },
         {
           title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          // imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
           id: 3,
           linkUrl: "shop/sneakers",
         },
         {
           title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          // imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           size: "large",
           id: 4,
           linkUrl: "shop/womens",
         },
         {
           title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          // imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
           size: "large",
           id: 5,
           linkUrl: "shop/mens",
@@ -46,8 +46,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, id }) => (
-          <MenuItem key={id} title={title} />
+        {this.state.sections.map(({ title, id, imageUrl, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
     );
