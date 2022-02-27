b80f19d75ecf01401483c5890ad74f9e9d9e5f86
Anya <annagapyuk@gmail.com>
2022-02-26 23:43:00 -0500

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-
   module: {
     rules: [
       {
